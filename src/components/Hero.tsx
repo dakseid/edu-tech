61d398083dd2a27b74f0e6ac0ad78413bea0b96d
Esaias Akinyemi <esaiasakinyemi@gmail.com>
2025-09-22 01:31:20 +0100

    css update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { motion } from 'framer-motion';
 
 const Hero = () => {
@@ -26,14 +25,29 @@ const Hero = () => {
         whileHover={{ scale: 1.05 }}
         whileTap={{ scale: 0.95 }}
         >
-            Get Started →
+            Get Started
         </motion.button>
 
+    <div className="moving-container">
         <div className="hero-image">
-          <img src="/path/to/your/image.jpg" alt="Hero" />
-          <div className="hero-image-overlay"></div>
-          
+            <div>
+          <img src="https://i.pinimg.com/736x/eb/76/a4/eb76a46ab920d056b02d203ca95e9a22.jpg" alt="Hero" />
+            </div>
+            <div>
+          <img src="https://i.pinimg.com/1200x/46/4b/8a/464b8ac1969f591a2316c8fa1d534027.jpg" alt="Hero" />
+            </div>
+            <div>
+          <img src="https://i.pinimg.com/736x/22/e5/9f/22e59f89cc596c374501270f83a1989f.jpg" alt="Hero" />
+            </div>
+            <div>
+          <img src="https://i.pinimg.com/736x/79/c6/9b/79c69b4aa810869c13b2d26a86fce54f.jpg" alt="Hero" />
+            </div>
+            <div>
+          <img src="https://i.pinimg.com/736x/eb/d3/f3/ebd3f3f26769de312f82f2c5c623023a.jpg" alt="Hero" />
+            </div>
+           
         </div>
+    </div>
     </section>
     );
 };
