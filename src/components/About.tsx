ccbc78ee66718f8ee9fe5e0ed44b506b1c9d78d2
Esaias Akinyemi <esaiasakinyemi@gmail.com>
2025-09-25 23:31:26 +0100

    about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import { FaAirbnb, FaAmazon, FaApple, FaAppStore, FaBehance, FaDraft2Digital, FaFutbol, FaGoogle, FaMicrosoft, FaSkull, FaStripe, FaTiktok, FaTwitch, FaTwitter } from "react-icons/fa";
+import { FaAirbnb, FaAmazon, FaApple, FaAppStore, FaDraft2Digital, FaFutbol, FaGoogle, FaMicrosoft, FaSkull, FaStripe, FaTiktok, FaTwitch, FaTwitter } from "react-icons/fa";
 import { Fa42Group, FaX } from "react-icons/fa6";
 
 
@@ -21,37 +21,34 @@ const About = () => {
         our products, solutions and bespoke software development services.
       </p>
 
-      <div className="about-icons" style={{ display: "flex", gap: "2rem", justifyContent: "center", marginTop: "2rem" }}>
+      <div className="about-icons">
         <div className="icon-row">
-        <FaApple size={40} color="#333" />
-        <FaGoogle size={40} color="#4285F4" />
-        <FaMicrosoft size={40} color="#F25022" />
-        <FaAmazon size={40} color="#FF9900" />
-        <FaTiktok size={40} color="#E82127" />
-        <FaSkull size={40} color="#000000" />
-        <FaTwitter size={40} color="#1DA1F2" />
-        <FaTwitch size={40} color="#9146FF" />
+        <FaApple size={50} color="#333" />
+        <FaGoogle size={50} color="#4285F4" />
+        <FaMicrosoft size={50} color="#F25022" />
+        <FaAmazon size={50} color="#FF9900" />
+        <FaTiktok size={50} color="#000000ff" />
+        <FaSkull size={50} color="#000000" />
+        <FaTwitter size={50} color="#1DA1F2" />
+        <FaTwitch size={50} color="#9146FF" />
         </div>
         <div className="icon-row2">
-        <FaDraft2Digital size={40} color="#FF6F61" />
-        <FaStripe size={40} color="#008CDD" />
-        <FaAirbnb size={40} color="#FF5A5F" />
-        <Fa42Group size={40} color="#000000" />
-        <FaAppStore size={40} color="#0A84FF" />
-        <FaX size={40} color="#000000" />
-        <FaFutbol size={40} color="#FF4500" />
-        <FaBehance size={40} color="#1769FF" />
+        <FaDraft2Digital size={50} color="#FF6F61" />
+        <FaStripe size={50} color="#008CDD" />
+        <FaAirbnb size={50} color="#ff0e16ff" />
+        <Fa42Group size={50} color="#000000" />
+        <FaAppStore size={50} color="#0A84FF" />
+        <FaX size={50} color="#000000" />
+        <FaFutbol size={50} color="#000000ff" />
         </div>
 
         <div className="icon-row3">
-        <FaAmazon size={40} color="#FF9900" />
-        <FaGoogle size={40} color="#4285F4" />
-        <FaMicrosoft size={40} color="#F25022" />
-        <FaApple size={40} color="#333" />
-        <FaTiktok size={40} color="#E82127" />
-        <FaSkull size={40} color="#000000" />
-        <FaTwitter size={40} color="#1DA1F2" />
-        <FaTwitch size={40} color="#9146FF" />
+        <FaAmazon size={50} color="#FF9900" />
+        <FaGoogle size={50} color="#4285F4" />
+        <FaMicrosoft size={50} color="#071ff7ff" />
+        <FaApple size={50} color="#333" />
+        <FaTiktok size={50} color="#000000ff" />
+        <FaSkull size={50} color="#000000" />
         </div>
 
 
